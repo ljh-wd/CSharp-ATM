3c648dc080c57ede40f2d1149627aca791857122
Luke <93337982+ljh-wd@users.noreply.github.com>
2023-05-15 20:20:26 +0100

    Added the ablity to enter your PIN and check if it’s correct or not.

---
+++ CONSOLE-APP-ATM/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string user = "Luke";
+            int pin = 2023;
+            double amount = 0.00;
+
+            Console.WriteLine("Hi " + user + ": Please enter your PIN number");
+
+            string checkPin = Console.ReadLine();
+
+            if (checkPin == pin.ToString())
+            {
+                Console.WriteLine("Balance: " + amount.ToString("F2"));
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Incorrect PIN");
+            }
+
+            Console.ReadLine(); // Keeps the console window open
+        }
+    }
+}
